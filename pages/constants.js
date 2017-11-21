2fbed06c207351a9d0a0e845355bfd37a34701ec
ludy.hxy <ludy.hxy@alibaba-inc.com>
2017-11-22 01:04:43 +0800

    fix: fix react jsfiddle demo

--- pages/constants.js
+++ pages/constants.js
@@ -1,8 +1,9 @@
 export const URL = {
-  react: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react.min.js',
-  reactDom: 'https://cdnjs.cloudflare.com/ajax/libs/react/15.1.0/react-dom.min.js',
+  react: 'https://unpkg.com/react@15/dist/react.min.js',
+  reactDom: 'https://unpkg.com/react-dom@15/dist/react-dom.min.js',
   vue: 'https://viserjs.github.io/lib/vue.min.js',
-  viser: 'https://viserjs.github.io/lib/viser.min.js',
-  viserReact: 'https://viserjs.github.io/lib/viser-react.min.js',
-  rechartVue: 'https://viserjs.github.io/lib/viser-vue.min.js',
+  browser: 'https://cdn.bootcss.com/babel-core/5.8.38/browser.min.js',
+  viser: 'https://unpkg.com/viser@1.0.3/umd/viser.min.js',
+  viserReact: 'https://unpkg.com/viser-react@1.0.1/umd/viser-react.min.js',
+  rechartVue: 'https://unpkg.com/viser-react@1.0.1/umd/viser-vue.min.js',
 };
