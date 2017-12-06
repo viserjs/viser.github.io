8be6549a719bfa9f9ec32c72b5bbd73ffd973506
evilucifero <x@evilucifero.com>
2017-12-06 14:06:00 +0800

    feat: add rose example

---
+++ pages/demo/examples/rose/example2/angular.ts
@@ -0,0 +1,68 @@
+import 'zone.js';
+import 'reflect-metadata';
+import { Component, enableProdMode, NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserModule } from '@angular/platform-browser';
+import { ViserModule } from 'viser-ng';
+
+const data = [
+  { year: '2001', population: 41.8 },
+  { year: '2002', population: 38 },
+  { year: '2003', population: 33.7 },
+  { year: '2004', population: 30.7 },
+  { year: '2005', population: 25.8 },
+  { year: '2006', population: 31.7 },
+  { year: '2007', population: 33 },
+  { year: '2008', population: 46 },
+  { year: '2009', population: 38.3 },
+  { year: '2010', population: 28 },
+  { year: '2011', population: 42.5 },
+  { year: '2012', population: 30.3 }
+];
+
+@Component({
+  selector: '#mount',
+  template: `
+  <div>
+    <Chart [forceFit]="forceFit" [height]="height" [data]="data">
+      <Tooltip></Tooltip>
+      <Sector
+        [position]="'year*population'"
+        [style]="sectorStyle"
+        [color]="'year'"
+      ></Sector>
+      <Legend
+        [position]="'right'"
+        [dataKey]="'year'"
+        [offsetY]="-70"
+        [offsetX]="-140"
+      ></Legend>
+    </Chart>
+  </div>
+  `
+})
+class AppComponent {
+  forceFit: boolean = true;
+  height: number = 500;
+  data = data;
+  sectorStyle = {
+    stroke: "#fff",
+    lineWidth: 1
+  };
+}
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ViserModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
+platformBrowserDynamic().bootstrapModule(AppModule);
