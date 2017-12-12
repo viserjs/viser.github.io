d40a1e05d3e4d1fb9c757a542c3793d76f433dda
evilucifero <x@evilucifero.com>
2017-12-12 15:53:14 +0800

    fix: home coord bug

--- pages/home/index.ts
+++ pages/home/index.ts
@@ -5,16 +5,16 @@ import './index.scss';
 const ALL_PAGE_LANGUAGES = ['en', 'cn'];
 const DEFAULT_PAGE_LANGUAGE = 'en';
 const GDP_JSON = [
-  { "year": '2006', "gdp": 21.94385 },
-  { "year": '2007', "gdp": 27.02323 },
-  { "year": '2008', "gdp": 31.95155 },
-  { "year": '2009', "gdp": 34.90814 },
-  { "year": '2010', "gdp": 41.30303 },
-  { "year": '2011', "gdp": 48.93006 },
-  { "year": '2012', "gdp": 54.03674 },
-  { "year": '2013', "gdp": 59.52444 },
-  { "year": '2014', "gdp": 64.39740 },
-  { "year": '2015', "gdp": 68.90521 }
+  { year: '2006', gdp: 21.94385 },
+  { year: '2007', gdp: 27.02323 },
+  { year: '2008', gdp: 31.95155 },
+  { year: '2009', gdp: 34.90814 },
+  { year: '2010', gdp: 41.30303 },
+  { year: '2011', gdp: 48.93006 },
+  { year: '2012', gdp: 54.03674 },
+  { year: '2013', gdp: 59.52444 },
+  { year: '2014', gdp: 64.39740 },
+  { year: '2015', gdp: 68.90521 }
 ];
 
 class Home {
@@ -40,8 +40,17 @@ class Home {
     });
     Viser({
       data: GDP_JSON,
+      tooltip: true,
+      axis: true,
+      series: [{ quickType: 'area', color: '#0088fe', position: 'year*gdp' }],
+      chart: { width: 380, height: 230, container: 'viser-mount-2-3' },
+    });
+
+    Viser({
+      data: GDP_JSON,
       tooltip: { showTitle: false },
       axis: true,
+      coord: { type: 'theta' },
       series: [{
         quickType: 'pie',
         position: 'gdp',
@@ -57,14 +66,8 @@ class Home {
     Viser({
       data: GDP_JSON,
       tooltip: true,
-      axis: true,
-      series: [{ quickType: 'area', color: '#0088fe', position: 'year*gdp' }],
-      chart: { width: 380, height: 230, container: 'viser-mount-2-3' },
-    });
-    Viser({
-      data: GDP_JSON,
-      tooltip: true,
       axis: false,
+      coord: { type: 'polar' },
       series: [{
         quickType: 'sector',
         color: '#0088fe',
