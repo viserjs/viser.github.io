8a3c03364a4f9c5792734fb7359870d1a1cb017c
evilucifero <x@evilucifero.com>
2017-12-13 18:41:56 +0800

    fix: pie example 4 angular showTitle no effect

--- pages/demo/examples/pie/example4/angular.ts
+++ pages/demo/examples/pie/example4/angular.ts
@@ -63,7 +63,7 @@ const color = ['name', ['#BAE7FF', '#7FC9FE', '#71E3E3', '#ABF5F5', '#8EE0A1', '
   template: `
   <div>
     <v-chart [forceFit]="true" [height]="height" [data]="data" [dataPre]="dataPre" [scale]="scale">
-      <v-tooltip showTitle="false" [itemTpl]="itemTpl"></v-tooltip>
+      <v-tooltip [showTitle]="false" [itemTpl]="itemTpl"></v-tooltip>
       <v-coord type="theta" radius="{{0.5}}"></v-coord>
       <v-pie position="percent" color="type" [label]="label" select="false" [style]="style"></v-pie>
       <v-view [dataPre]="viewDataPre" [scale]="scale">
