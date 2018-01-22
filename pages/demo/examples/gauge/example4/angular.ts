a04a6d48fb4ae8869f7528b207d6882d1870eb4c
evilucifero <x@evilucifero.com>
2018-01-22 22:05:48 +0800

    fix: no animate in gauge

--- pages/demo/examples/gauge/example4/angular.ts
+++ pages/demo/examples/gauge/example4/angular.ts
@@ -68,7 +68,7 @@ const frontGuideStyle = {
   selector: '#mount',
   template: `
   <div>
-    <v-chart [forceFit]="forceFit" height="400" [scale]="scale">
+    <v-chart [forceFit]="forceFit" height="400" [scale]="scale" [animate]="false">
       <v-view [data]="dataBackground">
         <v-coord
           type="polar"
@@ -84,12 +84,12 @@ const frontGuideStyle = {
         ></v-interval>
       </v-view>
       <v-view [data]="dataBackground" [scale]="insideScale">
-        <v-axis dataKey="value" show="false"></v-axis>
+        <v-axis dataKey="value" [show]="false"></v-axis>
         <v-axis
           dataKey="type"
-          grid="null"
-          line="null"
-          tickLine="null"
+          [grid]="null"
+          [line]="null"
+          [tickLine]="null"
           [label]="insideAxisLabel"
         ></v-axis>
         <v-coord
