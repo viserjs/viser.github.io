83b41e6a24fe0f3cad4c69c6552e4e4866e0cf87
wb-hjh418165 <970489393@qq.com>
2019-04-29 20:49:01 +0800

    fix:demo from import 'zone.js';

--- pages/demo/examples/others/example24/angular.ts
+++ pages/demo/examples/others/example24/angular.ts
@@ -1,11 +1,11 @@
 import 'zone.js';
 import 'reflect-metadata';
-import { Component, enableProdMode, NgModule } from '@angular/core';
+import { Component,OnInit, enableProdMode, NgModule } from '@angular/core';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { ViserModule } from 'viser-ng';
 const data = [
-  {"title":"Revenue","subtitle":"US$, in thousands","ranges":[150,225,300],"actual":270,"target":250},
+  {"title":"Revenue","subtitle":"US$, in thousands","ranges":[150,225,300],"actual":270,"target":250, start: {}, end: {}, scale: [], guide: []},
   {"title":"Profit","subtitle":"%","ranges":[20,25,30],"actual":23,"target":26},
   {"title":"Order Size","subtitle":"US$, average","ranges":[350,500,600],"actual":100,"target":550},
   {"title":"New Customers","subtitle":"count","ranges":[1400,2000,2500],"actual":1650,"target":2100},
@@ -16,16 +16,6 @@ const scale = [{
   dataKey: 'population',
   tickInterval: 1000000,
 }];
-
-const colorMap = {
-  'Under 5 Years': '#E3F4BF',
-  '5 to 13 Years': '#BEF7C8',
-  '14 to 17 Years': '#86E6C8',
-  '18 to 24 Years': '#36CFC9',
-  '25 to 44 Years': '#209BDD',
-  '45 to 64 Years': '#1581E6',
-  '65 Years and Over': '#0860BF'
-};
 
 const legendItems = [
   {
@@ -58,28 +48,28 @@ const legendItems = [
   selector: '#mount',
   template: `
   <div>
-    <v-chart [forceFit]="forceFit" [height]="height" [padding]="[100, 150]" [data]="data" [scale]="scale">
+    <v-chart [forceFit]="forceFit" [height]="365" [padding]="[100, 150]" [data]="data" [scale]="scale">
       <v-tooltip></v-tooltip>
-      <v-legend [custom]="true" [clickable]="false" [items]="legendItems" ></v-legend>
-      <v-view *ngFor="let item of data; let i = index;" [start]="getStart(i)" [end]="getEnd(i)"
-        [data]="[item]" [scale]="this.getScale(item, i)">
+      <v-legend [custom]="true" [clickable]="false" [items]="legendItems"></v-legend>
+      <v-view *ngFor="let item of data; let i = index;" [start]="item['start']" [end]="item['end']"
+              [data]="[item]" [scale]="item['scale']">
         <v-coord type="rect" direction='LB'></v-coord>
         <v-axis dataKey="target" [show]="false"></v-axis>
         <v-axis dataKey="actual" position="right"></v-axis>
         <v-point position="title*target" color="#square" shape="line" [size]="12" [style]="{lineWidth: 2}"></v-point>
         <v-interval position="title*actual" color="#223273" [size]="15"></v-interval>
-        <v-guide type="region" [start]="this.getGuide(item, 0, 'start')" [end]="this.getGuide(item, 0, 'end')"
-          [style]="{
+        <v-guide type="region" [start]="item['guide'][0]['start']" [end]="item['guide'][0]['end']"
+                 [style]="{
             fill: '#FFA39E',
             fillOpacity: 0.85
           }"></v-guide>
-        <v-guide type="region" [start]="this.getGuide(item, 1, 'start')" [end]="this.getGuide(item, 1, 'end')"
-          [style]="{
+        <v-guide type="region" [start]="item['guide'][1]['start']" [end]="item['guide'][1]['end']"
+                 [style]="{
             fill: '#FFD591',
             fillOpacity: 0.85
           }"></v-guide>
-        <v-guide type="region" [start]="this.getGuide(item, 2, 'start')" [end]="this.getGuide(item, 2, 'end')"
-          [style]="{
+        <v-guide type="region" [start]="item['guide'][2]['start']" [end]="item['guide'][2]['end']"
+                 [style]="{
             fill: '#A7E8B4',
             fillOpacity: 0.85
           }"></v-guide>
@@ -90,45 +80,38 @@ const legendItems = [
   `
 })
 
-class AppComponent {
+class AppComponent  implements OnInit {
   forceFit: boolean= true;
   height: number = 400;
   data = data;
   scale = scale;
-  colorMap = colorMap;
   legendItems = legendItems;
   y = 0;
   yGap = 0.1;
-  getStart(i) {
-    const { y, yGap } = this;
-    return {x:0, y: y + i * yGap + i * 0.125};
-  }
-  getEnd(i) {
-    const { y, yGap } = this;
-    return {x: 1, y: y + (i+1) * yGap + i * 0.125};
-  }
-  getScale (item, i) {
-    const ranges = item.ranges;
-    return [{
-      dataKey: 'actual',
-      min: 0,
-      max: ranges[2],
-      nice: false
-    }, {
-      dataKey: 'target',
-      min: 0,
-      max: ranges[2],
-      nice: false
-    }];
-  }
-  getGuide(viewData, guideIndex, position) {
-    const ranges = viewData.ranges;
-    const guide = [
-      {start: [-1, 0], end:[1, ranges[0]]},
-      {start: [-1, ranges[0]], end: [1, ranges[1]]},
-      {start: [-1, ranges[1]], end: [1, ranges[2]]}
-    ];
-    return guide[guideIndex][position];
+  
+  ngOnInit() {
+    // 定义一个变量来保存这些信息或者直接写入数据中
+    this.data.forEach((value, index) => {
+      value.start = {x: 0, y: this.y + index * this.yGap + index * 0.125};
+      value.end = {x: 1, y: this.y + (index + 1) * this.yGap + index * 0.125};
+      value.scale = [{
+        dataKey: 'actual',
+        min: 0,
+        max: value.ranges[2],
+        nice: false
+      }, {
+        dataKey: 'target',
+        min: 0,
+        max: value.ranges[2],
+        nice: false
+      }];
+      value.guide = [
+        {start: [-1, 0], end: [1, value.ranges[0]]},
+        {start: [-1, value.ranges[0]], end: [1, value.ranges[1]]},
+        {start: [-1, value.ranges[1]], end: [1, value.ranges[2]]}
+      ];
+    });
+
   }
 }
 
@@ -146,4 +129,3 @@ class AppComponent {
   ]
 })
 export default class AppModule { }
-
